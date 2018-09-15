56ee43dd5f482c65ad4c54349298622f08bac6cb
R.Guo <ray.guo.inchina@gmail.com>
2018-09-15 22:53:24 +0800

    same questions need to be solved next time

---
+++ ftn95/module/mod_sharedata.f95
@@ -0,0 +1,30 @@
+module mod_sharedata
+! Q: how to reflect data share
+Implicit None
+real :: a = 0 , b = 0 , c = 0
+
+contains
+
+Subroutine fc_Init()
+a = 3 ; b = 4 ; c = 5
+end subroutine fc_Init
+
+end module mod_sharedata
+
+!------------------------------------
+
+Program main
+use mod_sharedata
+Implicit None
+write( * , * ) a , b , c !the initiation value
+
+call fc_Init()
+write( * , * ) a , b , c !subroutine value
+
+a = 1 ; b = 2 ; c = 3
+write( * , * ) a , b , c !main program value
+
+call fc_Init()
+write( * , * ) a , b , c !subroutine value
+
+End Program main
